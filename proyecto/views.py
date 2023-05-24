02d30185c4a3140977962a57945becdb4647b3a7
adan llanos <adanllanos96@gmail.com>
2023-05-24 12:14:45 -0400

    mejorando las rutas

--- proyecto/views.py
+++ proyecto/views.py
@@ -25,6 +25,10 @@ def usuarios(request):
     usuarios = registrar_usuario.objects.all()
     return render(request, "usuarios.html",{'usuarios':usuarios})
 
+@login_required
+def factura(request):
+    return render(request, "factura.html")
+
 
 @user_passes_test(lambda u: u.is_superuser, login_url='/403/')        
 
